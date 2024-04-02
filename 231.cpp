eddd0e535890e98eebcc7dbed27e8be377e62c6b
suraksha011 <115644943+suraksha011@users.noreply.github.com>
2024-04-02 19:23:50 +0530

    Create 231.cpp
    231. Power of Two
    Given an integer n, return true if it is a power of two. Otherwise, return false.
    
    An integer n is a power of two, if there exists an integer x such that n == 2x.
    
     
    
    Example 1:
    
    Input: n = 1
    Output: true
    Explanation: 20 = 1
    Example 2:
    
    Input: n = 16
    Output: true
    Explanation: 24 = 16
    Example 3:
    
    Input: n = 3
    Output: false
     
    
    Constraints:
    
    -231 <= n <= 231 - 1

---
+++ 231.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int rem;
+        if(n<1){
+            return 0;
+        }
+        while(n!=1){
+            if(n%2==1){return 0;}
+            n/=2;
+        }
+        return 1;
+    }
+};
