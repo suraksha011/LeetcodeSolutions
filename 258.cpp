5bd48dff61b65f13139bb9daf8b1693a24409f04
suraksha011 <115644943+suraksha011@users.noreply.github.com>
2024-04-02 19:01:27 +0530

    Create 258.cpp
    258. Add Digits
     Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
    
     
    
    Example 1:
    
    Input: num = 38
    Output: 2
    Explanation: The process is
    38 --> 3 + 8 --> 11
    11 --> 1 + 1 --> 2 
    Since 2 has only one digit, return it.
    Example 2:
    
    Input: num = 0
    Output: 0
     
    
    Constraints:
    
    0 <= num <= 231 - 1

---
+++ 258.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+
+        while(num>9){
+            int rem,ans=0;
+            while(num!=0){
+                rem=num%10;
+                num=num/10;
+                ans=ans+rem;
+            }
+            num=ans;
+        }
+        return num;
+    }
+};
